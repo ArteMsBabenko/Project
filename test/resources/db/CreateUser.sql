7d52f5453c8a2c275ad742d857ec8a99713181a8
ArteMsBabenko <106840102+ArteMsBabenko@users.noreply.github.com>
2023-01-27 22:27:21 +0200

    Delete test directory

--- test/resources/db/CreateUser.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user VALUES
-(2, 'Vasily', 'Ivanov', 'ivanov@mail.com', '$2a$08$GqFnDNRtlw3Ib1bdM7BzSesSALwiMOFSwSewHNflrTB5IDaug6AQ6', TRUE, NULL);
-
-INSERT INTO access_level VALUES
-(2, 'USER');
